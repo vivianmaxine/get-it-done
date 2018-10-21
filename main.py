7cfbd18ed5d4243f2bdb20ac405e516fa557c2f9
Vivian Crosby <design@vivianmaxine.com>
2018-10-20 23:43:55 -0500

    Set up login and register handlers after creating login.html and register.html

--- main.py
+++ main.py
@@ -33,6 +33,16 @@ class Task(db.Model):
         self.completed = False  # False if NOT completed (default)
 
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(120))
+
+    def __init__(self, email, password):
+        self.email = email
+        self.password = password
+
+
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
@@ -50,8 +60,11 @@ def index():
 
     completed_tasks = Task.query.filter_by(completed=True).all()
 
-    return render_template('todos.html', title="Get It Done!",
-            tasks=tasks, completed_tasks=completed_tasks)
+    return render_template(
+        'todos.html', title="Get It Done!",
+        tasks=tasks, completed_tasks=completed_tasks)
+    # todos.html is a positional argument
+    # tasks=tasks is a key word argument
 
 # refers to form in todos.html where you select a task to delete it
 
@@ -68,6 +81,62 @@ def delete_task():
     # db.session.commit()
 
     return redirect('/')
+
+
+# REGISTER HANDLER
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        new_email = request.form['email']
+        new_password = request.form['password']
+        new_password_verify = request.form['verify']
+
+        existing_user = User.query.filter_by(email=new_email).first()
+
+        if not existing_user:
+
+            if new_password == new_password_verify:
+                new_user = User(new_email, new_password)
+
+                db.session.add(new_user)
+                db.session.commit()
+
+                # TODO: REMEMBER THE USER AS LOGGED IN
+                return redirect('/')
+            else:
+                return '<h1 style="color: red">Please make sure \
+                your passwords match.</h1>'
+        else:
+            return '<h1 style="color: red">This user already exists.</h1>'
+
+    return render_template('register.html')
+
+
+# LOGIN HANDLER
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    # first, check for request type;
+    # GET just return form, POST needs to get data
+    if request.method == 'POST':
+        login_email = request.form['email']
+        login_password = request.form['password']
+
+        # verify password by retrieving user with given email from db
+        # and seeing if it equals the enteres password
+        user = User.query.filter_by(email=login_email).first()
+
+        # below, "if user" checks to see if above condition met
+        # below, "user.password == password" checks to see if
+        # entered pw = db pw
+        if user and user.password == login_password:
+            # TODO - REMEMBER THAT USER HAS LOGGED IN
+            return redirect('/')
+        else:
+            # TODO - ERROR MESSAGES EXPLAINING WHY LOGIN FAILED
+            return '<h1 style="color:red">ERROR ALERT!<br> \
+            Email or Password Incorrect! :(</h1>'
+    return render_template('login.html')
+
 
 if __name__ == "__main__":
     app.run()
